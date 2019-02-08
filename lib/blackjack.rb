e620f2193f260c9bbea668bde0bca7bf798eee7f
Alicia Santiago <albrown07@mac.com>
2019-02-08 18:26:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,7 +55,7 @@ def runner
  card_total = initial_round
  hit?
  display_card_total
-  until card_total > 21 do
+  until card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
